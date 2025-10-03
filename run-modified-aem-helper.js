d939743e1c1173a564e5130be0d3fd0c1136ef00
meejain <meetkjain83@gmail.com>
2025-10-03 20:41:29 +0530

    Adding files

--- run-modified-aem-helper.js
+++ run-modified-aem-helper.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Path to our modified AEM import helper
-const modifiedHelperPath = path.join(__dirname, 'aem-import-helper-working', 'src', 'bin.js');
+const modifiedHelperPath = path.join(__dirname, 'src', 'bin.js');
 
 // Command arguments
 const args = [
